71fc51bf72c80ef02201863a7b5b7119d4f74860
Gomolemo-k <kgatitsoeg321@gmail.com>
2025-07-21 23:41:12 +0200

    moved to pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-  },
+  // These belong at the top level, not inside 'experimental'
+  runtime: 'nodejs',
+  optimizeCss: false,
+  optimizeFonts: false,
 }
 
 module.exports = nextConfig;
