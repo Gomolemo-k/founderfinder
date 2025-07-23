586971b5bb801bd961249bba54f7eec2c4d0554b
Gomolemo-k <kgatitsoeg321@gmail.com>
2025-07-23 11:19:34 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    BASE_URL: process.env.BASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+  },
 };
 
 module.exports = nextConfig;
