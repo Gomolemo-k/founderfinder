4ddbb32dfdc27228ec3be61f1354d13793c9c9df
Gomolemo-k <kgatitsoeg321@gmail.com>
2025-07-24 11:26:05 +0200

    added runtime

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,10 +2,10 @@ import { getUser } from '@/lib/db/queries';
 import { getDb } from '@/lib/db/drizzle';
 import { cookies } from 'next/headers';
 
-export const runtime = 'edge'; 
+export const runtime = 'nodejs'; 
 
-export async function GET(_: Request, context: { env: { DB: D1Database } }) {
-  const db = getDb(context.env.DB);
+export async function GET() {
+  const db = getDb(process.env.DB as unknown as D1Database); 
   const user = await getUser(db, cookies());
   return Response.json(user ?? {});
 }
