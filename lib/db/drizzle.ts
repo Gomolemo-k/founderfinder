8b6f9b9089f9fb4b21d51d41d7b9a9247a0edfbe
Gomolemo <kgatitsoeg321@gmial.com>
2025-08-08 11:54:26 +0200

    fixed drizzle

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -1,6 +1,13 @@
-import { drizzle } from 'drizzle-orm/d1';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 import * as schema from './schema';
+import dotenv from 'dotenv';
 
-export function getDb(d1: D1Database) {
-  return drizzle(d1, { schema });
+dotenv.config();
+
+if (!process.env.POSTGRES_URL) {
+  throw new Error('POSTGRES_URL environment variable is not set');
 }
+
+export const client = postgres(process.env.POSTGRES_URL);
+export const db = drizzle(client, { schema });
