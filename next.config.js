b75633fe05bca67bc5053bd8faff26f4914b42fe
Gomolemo <kgatitsoeg321@gmial.com>
2025-08-13 19:01:15 +0200

    added next config .js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    ppr: true,
+    clientSegmentCache: true,
+    nodeMiddleware: true
+  }
+};
+
+module.exports = nextConfig;
