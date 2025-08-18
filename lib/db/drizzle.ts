676f222ee3feaf47a40ff88602d4260954efaa22
Gomolemo <kgatitsoeg321@gmial.com>
2025-08-18 12:03:17 +0200

    fixed drizzle

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -4,5 +4,4 @@ export interface Env {
   DB: D1Database;
 }
 
-// Accept env as a parameter to ensure correct type
-export const getDb = (env: Env) => drizzle(env.DB);
+export const db = (env: Env) => drizzle(env.DB);
